8b34f0bfa23cd5ba63fd87b8191abe715b8df964
hlam <hoanglam9925@gmail.com>
2024-11-06 16:07:54 +0700

    Revert "Change message list to flat list"
    This reverts commit d2fafc6482b6fce09bfef3b78b743c26d27cad78.

--- src/components/message-list/index.tsx
+++ src/components/message-list/index.tsx
@@ -7,7 +7,6 @@ import MessageType from '../../types/MessageType'
 import TypingIndicator from '../typing-indicator'
 import MessageListBackground from '../message-list-background'
 import useColorSet from '../../hooks/useColorSet'
-import FlatList from 'flatlist-react'
 
 export type MessageListProps = {
     themeColor?: string
@@ -20,11 +19,77 @@ export type MessageListProps = {
     typingIndicatorContent?: string
     customTypingIndicatorComponent?: React.ReactNode
     customEmptyMessagesComponent?: React.ReactNode
-    customLoaderComponent?: React.ReactNode
+    customLoaderComponent?: React.ReactNode,
 }
 
-const MessageList: React.FC<MessageListProps> = ({
-    messages = [], // Default to empty array if undefined
+
+
+const Container = styled.div`
+height: 100%;
+/* display: flex;
+flex-direction: column; */
+position: relative;
+max-height: 100vh;
+overflow-y: hidden;
+/* background-color: #ffffff; */
+padding-left: 0px;
+padding-right: 12px; 
+`
+
+const InnerContainer = styled.div`
+height: 100%;
+`
+
+
+const ScrollContainer = styled.div`
+overflow-y: auto;
+position: relative;
+height: 100%;
+width: 100%;
+max-height: 100vh;
+box-sizing: border-box;
+display: flex;
+flex-direction: column;
+scrollbar-width: none; /* Firefox */
+ -ms-overflow-style: none;  /* Internet Explorer 10+ */
+::-webkit-scrollbar { /* WebKit */
+    width: 0;
+    height: 0;
+}
+`
+
+const Buffer = styled.div`
+    height: 2px;
+    width: 100%;
+    position: relative;
+`
+
+const NoMessagesTextContainer = styled.div<{
+    color?: string
+}>`
+  color:${({ color }) => color || 'rgba(0,0,0,.36)'};
+  font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  font-size:14px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    user-select: none;
+
+`
+
+const LoadingContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+position: relative;
+`
+
+export default function MessageList({
+    messages,
     currentUserId,
     loading = false,
     onScrollToTop,
@@ -35,43 +100,72 @@ const MessageList: React.FC<MessageListProps> = ({
     customTypingIndicatorComponent,
     customLoaderComponent,
     customEmptyMessagesComponent,
-}) => {
-    const [messagesWasEmpty, setMessagesWasEmpty] = useState<boolean>(true);
-    const containerRef = useRef<HTMLDivElement | null>(null);
-    const bottomBufferRef = useRef<HTMLDivElement | null>(null);
-    const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+}: MessageListProps) {
 
-    const { detectBottom, detectTop } = useDetectScrollPosition(scrollContainerRef);
+    /** keeps track of whether messages was previously empty or whether it has already scrolled */
+    const [messagesWasEmpty, setMessagesWasEmpty] = useState(true)
+    const containerRef = useRef<any>()
+
+    const bottomBufferRef = useRef<any>()
+    const scrollContainerRef = useRef<any>()
+
+    const { detectBottom, detectTop } = useDetectScrollPosition(scrollContainerRef)
+
 
     useEffect(() => {
+        //detecting when the scroll view is first rendered and messages have rendered then you can scroll to the bottom
         if (bottomBufferRef.current && scrollContainerRef.current && !messagesWasEmpty) {
-            scrollToBottom();
+            scrollToBottom()
         }
-    }, [messagesWasEmpty]);
+
+    }, [messagesWasEmpty, bottomBufferRef.current, scrollContainerRef.current])
+
 
     useEffect(() => {
-        if (!messages || messages.length === 0) {
-            setMessagesWasEmpty(true);
-        } else {
-            if (messagesWasEmpty) {
-                setMessagesWasEmpty(false);
-                scrollToBottom();
-            }
-            if (detectBottom()) {
-                scrollToBottom();
-            }
+        if (!messages) {
+            setMessagesWasEmpty(true)
         }
-    }, [messages]);
 
-    const noMessageTextColor = useColorSet("--no-message-text-color");
+        if (messages) {
+            if (messagesWasEmpty) {
+                //if the messages object was previously empty then scroll to bottom
+                // this is for when the first page of messages arrives
+                //if a user has instead scrolled to the top and the next page of messages arrives then don't scroll to bottom
 
-    const scrollToBottom = () => {
+                setMessagesWasEmpty(false)
+                scrollToBottom()
+            }
+
+            // when closer to the bottom of the scroll bar and a new message arrives then scroll to bottom
+            if (detectBottom()) {
+                scrollToBottom()
+            }
+
+            scrollToBottom()
+
+        }
+    }, [messages])
+
+
+    useEffect(() => {
+        //TODO when closer to the bottom of the scroll bar and a new message arrives then scroll to bottom
+        if (detectBottom()) {
+            scrollToBottom()
+        }
+    }, [showTypingIndicator])
+
+
+    const noMessageTextColor = useColorSet("--no-message-text-color")
+
+    const scrollToBottom = async () => {
         if (bottomBufferRef.current && scrollContainerRef.current) {
-            const container = scrollContainerRef.current;
-            const scrollPoint = bottomBufferRef.current;
+            const container = scrollContainerRef.current
+            const scrollPoint = bottomBufferRef.current
 
-            const parentRect = container.getBoundingClientRect();
-            const childRect = scrollPoint.getBoundingClientRect();
+            const parentRect = container.getBoundingClientRect()
+            const childRect = scrollPoint.getBoundingClientRect()
+
+            // Scroll by offset relative to parent
             const scrollOffset = childRect.top + container.scrollTop - parentRect.top;
 
             if (container.scrollBy) {
@@ -80,51 +174,75 @@ const MessageList: React.FC<MessageListProps> = ({
                 container.scrollTop = scrollOffset;
             }
         }
-    };
+    }
+
+
 
     return (
-        <Container ref={containerRef}>
-            <MessageListBackground roundedCorners={false} mobileView={mobileView} />
+        <Container
+            ref={containerRef}
+        >
+
+            <MessageListBackground
+                roundedCorners={false}
+                mobileView={mobileView} />
+
 
             <InnerContainer>
-                {loading ? (
-                    <LoadingContainer>
-                        {customLoaderComponent ? customLoaderComponent : <Loading themeColor={themeColor} />}
-                    </LoadingContainer>
-                ) : (
-                    <FlatList
-                        list={messages.map((message, index) => ({ item: message, index }))} // Map messages to required format
-                        ref={scrollContainerRef}
-                        renderWhenEmpty={() => {
-                            if (customEmptyMessagesComponent) return customEmptyMessagesComponent as React.ReactElement;
-                            return (
-                                <NoMessagesTextContainer color={noMessageTextColor}>
-                                    <p>No messages yet...</p>
-                                </NoMessagesTextContainer>
-                            );
-                        }
 
-                        }
-                        renderItem={({ item, index }: { item: MessageType; index: number }) => {
-                            const { user, text, media, loading: messageLoading, failed: messageFailed, seen, createdAt, debugInfo } = item;
-                            
-                            let lastClusterMessage = false, firstClusterMessage = false, last = false, single = false;
-                    
-                            // Determine message cluster positions
-                            if (index === 0) firstClusterMessage = true;
-                            if (index > 0 && messages[index - 1].user.id !== user.id) firstClusterMessage = true;
-                            if (index === messages.length - 1) { lastClusterMessage = true; last = true; }
-                            if (index < messages.length - 1 && messages[index + 1].user.id !== user.id) { lastClusterMessage = true; last = true; }
-                            if (index < messages.length - 1 && index > 0 && messages[index + 1].user.id !== user.id && messages[index - 1].user.id !== user.id) single = true;
-                            if (index === 0 && index < messages.length - 1 && messages[index + 1].user.id !== user.id) single = true;
-                            if (index === messages.length - 1 && index > 0 && messages[index - 1].user.id !== user.id) single = true;
-                            if (messages.length === 1) single = true;
-                    
-                            // Render outgoing message
-                            if (user.id === (currentUserId && currentUserId.toLowerCase())) {
-                                return (
-                                    <Message
-                                        key={index}
+                {loading ?
+                    <LoadingContainer>
+                        {customLoaderComponent ?
+                            customLoaderComponent :
+                            <Loading themeColor={themeColor} />}
+                    </LoadingContainer>
+                    :
+                    <>
+
+                        <ScrollContainer
+                            onScroll={() => {
+                                //detect when scrolled to top
+                                if (detectTop()) {
+                                    onScrollToTop && onScrollToTop()
+                                }
+                            }}
+                            ref={scrollContainerRef}>
+
+                            {(messages && messages.length <= 0) &&
+                                (customEmptyMessagesComponent ?
+                                    customEmptyMessagesComponent
+                                    :
+                                    <NoMessagesTextContainer
+                                        color={noMessageTextColor}>
+                                        <p>No messages yet...</p>
+                                    </NoMessagesTextContainer>)
+                            }
+                            {messages && scrollContainerRef.current && bottomBufferRef.current && messages.map(({ user, text, media, loading: messageLoading, failed: messageFailed, seen, createdAt, debugInfo }, index) => {
+                                //determining the type of message to render
+                                let lastClusterMessage, firstClusterMessage, last, single
+
+                                //if it is the first message in the messages array then show the header
+                                if (index === 0) { firstClusterMessage = true }
+                                //if the previous message from a different user then show the header
+                                if (index > 0 && messages[index - 1].user.id !== user.id) { firstClusterMessage = true }
+                                //if it is the last message in the messages array then show the avatar and is the last incoming
+                                if (index === messages.length - 1) { lastClusterMessage = true; last = true }
+                                //if the next message from a different user then show the avatar and is last message incoming
+                                if (index < messages.length - 1 && messages[index + 1].user.id !== user.id) { lastClusterMessage = true; last = true }
+                                //if the next message and the previous message are not from the same user then single incoming is true
+                                if (index < messages.length - 1 && index > 0 && messages[index + 1].user.id !== user.id && messages[index - 1].user.id !== user.id) { single = true }
+                                //if it is the first message in the messages array and the next message is from a different user then single incoming is true
+                                if (index === 0 && index < messages.length - 1 && messages[index + 1].user.id !== user.id) { single = true }
+                                //if it is the last message in the messages array and the previous message is from a different user then single incoming is true
+                                if (index === messages.length - 1 && index > 0 && messages[index - 1].user.id !== user.id) { single = true }
+                                //if the messages array contains only 1 message then single incoming is true
+                                if (messages.length === 1) { single = true }
+
+
+                                if (user.id == (currentUserId && currentUserId.toLowerCase())) {
+
+                                    // my message
+                                    return <Message key={index}
                                         type="outgoing"
                                         last={single ? false : last}
                                         single={single}
@@ -132,18 +250,19 @@ const MessageList: React.FC<MessageListProps> = ({
                                         seen={seen}
                                         created_at={createdAt}
                                         media={media}
+                                        // the last message should show loading if sendMessage loading is true
                                         loading={messageLoading}
                                         failed={messageFailed}
                                         clusterFirstMessage={firstClusterMessage}
                                         clusterLastMessage={lastClusterMessage}
                                         debugInfo={debugInfo}
                                     />
-                                );
-                            } else {
-                                // Render incoming message
-                                return (
-                                    <Message
-                                        type="incoming"
+
+                                } else {
+
+                                    // other message
+                                    return <Message
+                                        type='incoming'
                                         key={index}
                                         user={user}
                                         media={media}
@@ -156,72 +275,27 @@ const MessageList: React.FC<MessageListProps> = ({
                                         text={text}
                                         debugInfo={debugInfo}
                                     />
-                                );
-                            }
-                        }}
-                        onScroll={() => {
-                            if (detectTop()) {
-                                onScrollToTop && onScrollToTop();
-                            }
-                        }}
-                        footer={() => (
-                            <>
-                                {showTypingIndicator && (
-                                    customTypingIndicatorComponent ? (
-                                        customTypingIndicatorComponent
-                                    ) : (
-                                        <TypingIndicator content={typingIndicatorContent} themeColor={themeColor} />
-                                    )
-                                )}
+                                }
+                            })}
+
+                            {showTypingIndicator && (
+                                customTypingIndicatorComponent ?
+                                    customTypingIndicatorComponent
+                                    : <TypingIndicator
+                                        content={typingIndicatorContent}
+                                        themeColor={themeColor} />
+                            )}
+
+                            {/* bottom buffer */}
+                            <div>
                                 <Buffer ref={bottomBufferRef} />
-                            </>
-                        )}
-                    />
-                )}
+                            </div>
+                        </ScrollContainer>
+                    </>
+
+                }
             </InnerContainer>
+
         </Container>
-    );
+    )
 }
-
-export default MessageList;
-
-// Styled components
-const Container = styled.div`
-    height: 100%;
-    position: relative;
-    max-height: 100vh;
-    overflow-y: hidden;
-    padding-left: 0px;
-    padding-right: 12px;
-`;
-
-const InnerContainer = styled.div`
-    height: 100%;
-`;
-
-const NoMessagesTextContainer = styled.div<{ color?: string }>`
-    color: ${({ color }) => color || 'rgba(0,0,0,.36)'};
-    font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-    font-size: 14px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    user-select: none;
-`;
-
-const LoadingContainer = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 1;
-    position: relative;
-`;
-
-const Buffer = styled.div`
-    height: 2px;
-    width: 100%;
-    position: relative;
-`;
